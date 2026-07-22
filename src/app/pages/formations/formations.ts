bc48d5999c98283d9441233cc675274192ca294c
Mariam <mariamomri.dev@gmail.com>
2026-07-23 00:26:41 +0200

    add track with css for the formations array

--- src/app/pages/formations/formations.ts
+++ src/app/pages/formations/formations.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import { NgClass } from "@angular/common" ;
 @Component({
   selector: 'app-formations',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './formations.html',
   styleUrl: './formations.css',
 })
-export class Formations {}
+export class Formations {
+
+  formations =  [ 'Angular', 'React', 'Node.js', 'Java Spring', '.NET' ];
+  
+}
