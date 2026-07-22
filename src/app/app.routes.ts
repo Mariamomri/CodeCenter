a3161ce5d2f1530095a3655185a04b056f88e446
Mariam <mariamomri.dev@gmail.com>
2026-07-22 14:37:49 +0200

    feat: configure routes for accueil, formations, inscription, profil, dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { Formations } from './pages/formations/formations';
+import { Inscription } from './pages/inscription/inscription';
+import { Profil } from './pages/profil/profil';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Accueil },
+  { path: 'formations', component: Formations },
+  { path: 'inscription', component: Inscription },
+  { path: 'profil', component: Profil },
+  { path: 'dashboard', component: Dashboard },
+];
