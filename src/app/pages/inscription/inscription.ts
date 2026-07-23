46c26339c3e2dfe7027c6224ff8bcb41e53f36bf
Mariam <mariamomri.dev@gmail.com>
2026-07-23 02:10:00 +0200

    feat(inscription): add stagiaire service and wire form with two-way ngModel binding

--- src/app/pages/inscription/inscription.ts
+++ src/app/pages/inscription/inscription.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component , inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Stagiaire } from '../../services/stagiaire';
 
 @Component({
   selector: 'app-inscription',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inscription.html',
   styleUrl: './inscription.css',
+
 })
-export class Inscription {}
+export class Inscription {
+
+  service = inject(Stagiaire);
+
+  valider() {
+  console.log('Inscription validée', this.service.nom, this.service.prenom);
+}
+}
